a00e98297e938e15c35d137ccbcc4e50eca4d90c
00010100 <e0001101004@gmail.com>
2020-03-13 11:14:04 +0200

    feat(teamActions): add team actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 const authAction = require('./auth')
 const userAction = require('./user')
+const teamAction = require('./team')
 
 module.exports = {
   authAction,
-  userAction
+  userAction,
+  teamAction
 }
