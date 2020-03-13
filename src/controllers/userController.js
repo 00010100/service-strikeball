b0d13753c07d3f89d8eb3a570540597e5065cb19
00010100 <e0001101004@gmail.com>
2020-03-13 10:48:28 +0200

    feat(userActions): add user actions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,5 @@
-const {UserModel} = require('../models')
-const {verifyToken, errorHandler} = require('../utils')
 const {authValidate, userValidate} = require('../validator')
-const {authAction} = require('../actions')
+const {authAction, userAction} = require('../actions')
 
 const {wrap} = require('../middlewares')
 
@@ -20,51 +18,53 @@ const login = wrap(async (req, res) => {
 })
 
 const getAllUsers = wrap(async (req, res) => {
-  // TODO - change
-  res.send(await UserModel.find())
+  const result = await userAction.getUsers()
+
+  res.send(result)
 })
 
 const getUserById = wrap(async (req, res) => {
-  const data = await userValidate.getById(req.params)
+  const result = await userValidate.getById(req.params)
 
-  res.send(data)
+  res.send(result)
 })
 
 const deleteUserById = wrap(async (req, res) => {
-  const data = await userValidate.deleteById(req.params)
+  const data = await userValidate.getById(req.params)
+  const result = await userAction.deleteById(data)
 
-  res.send(data)
+  res.send(result)
 })
 
-const confirmationUserEmail = async (req, res, next) => {
-  try {
-    const verified = verifyToken(req.params.token)
+// const confirmationUserEmail = async (req, res, next) => {
+//   try {
+//     const verified = verifyToken(req.params.token)
 
-    if (!verified) {
-      return errorHandler(next, {code: 404})
-    }
+//     if (!verified) {
+//       return errorHandler(next, {code: 404})
+//     }
 
-    const user = await UserModel.findById(verified.userId)
+//     const user = await UserModel.findById(verified.userId)
 
-    if (!user) {
-      // return res.status(404).json({error: 'User does not exist'})
-      return errorHandler(next, {code: 404})
-    }
+//     if (!user) {
+//       // return res.status(404).json({error: 'User does not exist'})
+//       return errorHandler(next, {code: 404})
+//     }
 
-    if (user.confirmed) {
-      // return res.status(400).json({error: 'User already confimed'})
-      return errorHandler(next, {code: 400})
-    }
+//     if (user.confirmed) {
+//       // return res.status(400).json({error: 'User already confimed'})
+//       return errorHandler(next, {code: 400})
+//     }
 
-    if (user.role === 'manager') {
-      await UserModel.updateOne({_id: verified.userId}, {confirmed: true})
-    }
+//     if (user.role === 'manager') {
+//       await UserModel.updateOne({_id: verified.userId}, {confirmed: true})
+//     }
 
-    res.status(200).json({data: user, message: 'This manager was confirmed'})
-  } catch (err) {
-    errorHandler(next)
-  }
-}
+//     res.status(200).json({data: user, message: 'This manager was confirmed'})
+//   } catch (err) {
+//     errorHandler(next)
+//   }
+// }
 
 module.exports = {
   signUp,
@@ -72,5 +72,5 @@ module.exports = {
   getAllUsers,
   getUserById,
   deleteUserById,
-  confirmationUserEmail
+  // confirmationUserEmail
 }
